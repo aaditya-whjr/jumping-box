149bc8058147277ce1e66e2bcf60f80c1e31e1ba
aaditya-whjr <79003584+aaditya-whjr@users.noreply.github.com>
2021-04-25 10:37:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,81 +1,74 @@
-var tom;
-var jerry;
-var bgImg;
-var tomimg1,tomimg2,tomimg3;
-var jerryimg1,jerryimg2,jerryimg3;
+var canvas;
+var music;
+var ball;
+var surface1;
 
-function preload() {
-    bgImg=loadImage("images/garden.png");
-
-    tomimg1=loadAnimation("Images/cat1.png");
-    tomimg2=loadAnimation("Images/cat2.png","Images/cat3.png");
-    tomimg3=loadAnimation("Images/cat4.png");
-
-    jerryimg1=loadAnimation("Images/mouse1.png");
-    jerryimg2=loadAnimation("Images/mouse2.png","Images/mouse3.png");
-    jerryimg3=loadAnimation("Images/mouse4.png");
-
-
+function preload(){
+    music = loadSound("music.mp3");
 }
 
+
 function setup(){
-    createCanvas(1000,800);
-     tom = createSprite(870,600);
-     tom.addAnimation("tomSleeping",tomimg1);
-     tom.scale=0.2;
+    canvas = createCanvas(1550,750);
+    ball = createSprite(random(20,750),100,40,40);
+    ball.shapeColor="white";
+    ball.velocityX=4;
+    ball.velocityY=4;
 
-     jerry = createSprite(200,600);
-     jerry.addAnimation("jerryStanding",jerryimg1);
-     jerry.scale=0.15;
 
+    surface1=createSprite(150,700,200,30);
+    surface1.shapeColor="green";
+
+    surface2=createSprite(450,700,200,30);
+    surface2.shapeColor="red";
+
+    surface3=createSprite(750,700,200,30);
+    surface3.shapeColor="orange";
+
+    surface4=createSprite(1100,700,200,30);
+    surface4.shapeColor="black";
+
+    surface5=createSprite(1400,700,200,30);
+    surface5.shapeColor="blue";
 }
 
 function draw() {
-
-    background(bgImg);
-    //Write condition here to evalute if tom and jerry collide
-    if(tom.x - jerry.x < (tom.width - jerry.width)/2) {
-        tom.velocityX=0;
-        tom.addAnimation("tomlastimg",tomimg3);
-        tom.x300
-        tom.scale=0.2;
-        tom.changeAnimation("tomlastimg");
-        
-        jerry.addAnimation("jerrylastimg",jerryimg3);
-        jerry.scale=0.15;
-        jerry.changeAnimation("jerrylastimg");
-    }
-    drawSprites();
-}
-
-
-function keyPressed(){
-
-  //For moving and changing animation write code here
-  if(keyCode === LEFT_ARROW){
-    tom.velocityX=-5;
-    tom.frameDelay;
-    tom.addAnimation("tomrunningimg",tomimg2);
-    tom.changeAnimation("tomrunningimg");
-
-
-    jerry.addAnimation("jerryteasingimg",jerryimg2);
-    jerry.frameDelay=25;
-    jerry.changeAnimation("jerryteasingimg");
+    background(rgb(169,169,169));
     
+   edges=createEdgeSprites();
+
+   ball.bounceOff(edges);
+
+   if(surface1.isTouching(ball) && ball.bounceOff(surface1)){
+     ball.shapeColor="green";
+     music.play;
+   }
+
+   if(surface2.isTouching(ball) && ball.bounceOff(surface2)){
+    ball.shapeColor="red";
+    music.stop;
   }
 
-  if(keyCode === RIGHT_ARROW){
-    tom.velocityX=-5;
-    tom.addAnimation("tomrunningimg",tomimg2);
-    tom.changeAnimation("tomrunningimg");
-    tom.frameDelay=25;
-
-
-    jerry.addAnimation("jerryteasingimg",jerryimg2);
-    jerry.frameDelay=25;
-    jerry.changeAnimation("jerryteasingimg");
-    
+  if(surface3.isTouching(ball) && ball.bounceOff(surface3)){
+    ball.shapeColor="orange";
+    music.play;
+    ball.velocityX=0;
+    ball.velocityY=0;
   }
-  
+
+  if(surface4.isTouching(ball) && ball.bounceOff(surface4)){
+    ball.shapeColor="black";
+    music.play;
+    ball.velocityX=6;
+    ball.velocityY=6;
+  }
+
+  if(surface5.isTouching(ball) && ball.bounceOff(surface5)){
+    ball.shapeColor="blue";
+    music.stop;
+    ball.velocityX=0;
+    ball.velocityY=0;
+  }
+
+  drawSprites();
 }
